93f9ba207e42b672fc710c23a555252de05ceeb6
Jarrar Jaffari (jjaffari) <jjaffari@cisco.com>
2019-03-31 22:18:12 -0400

    Name of the plugin is chnaged

--- buildSrc/src/main/groovy/org/packaging/plugin/RpmExtension.groovy
+++ buildSrc/src/main/groovy/org/packaging/plugin/RpmExtension.groovy
@@ -3,4 +3,9 @@ package org.packaging.plugin
 class RpmExtension {
     String message
     String greeter
+
+    List<String> lib32Files
+    List<String> lib64Files
+    List<String> includFiles
+
 }
